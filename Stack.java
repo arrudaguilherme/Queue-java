30a560c5a972ac9370846447c77803c6736989c5
Guilherme Arruda <43161043+arrudaguilherme@users.noreply.github.com>
2019-07-15 18:35:18 -0700

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,38 +1,46 @@
-package pilha;
-import stack.Stack; //imports the object Stack(class stack) from the same package
+package queue;
+import queue.Node;
 
-public class Pilha<T> {
-    private Node <T> top;
+public class Fila<T> {
+    private Node<T>first;
+    private Node<T>last;
     
     public boolean isEmpty(){
-        if(this.top==null){
+        if(this.first==null){
             return true;
-            
         }else{
-            return false;
-        }
-    }    
+        return false;
+        }    
+    }
     
     public boolean isFull(){
-        return false;
+    return false;
+    }
+    public void enqueue(T valor){
+        Node<T> aux=new Node(valor);
+        if(this.isEmpty()==true){
+            this.first=aux;
+            this.last=aux;
+        }else{
+            this.last.setNext(aux);
+            aux.setPrev(this.last);
+            this.last=aux;
+        }    
         
     }
     
-    public void push(T valor){
-        Node <T> aux = new Node<T>(valor);
-        aux.setNext(this.top);
-        this.top=aux;
-        
-        
+    public T dequeue(){
+        Node<T>aux=this.first;
+        this.first=this.first.getNext();
+        this.first.setPrev(null);
+        if(this.isEmpty()==true){
+            this.last=null;
+        }
+        return aux.getInfo();
     }
     
-    public T top(){
-        return this.top.getInfo();
-    }    
-    
-    public T pop(){
-        Node <T> aux=this.top;
-        this.top=this.top.getNext();
+    public T head(){
+        Node<T> aux=this.first;
         return aux.getInfo();
     }
     
